69daf9598ea317c9ff2fe87791da456413310c61
tylerstauss <tylerstauss@Tylers-Mac-mini.local>
2013-10-22 16:04:11 -0700

    Fixed the validation
    Fixed number validation and between 1 and 100 validation.

--- guess.js
+++ guess.js
@@ -1,32 +1,67 @@
   var correct = Math.floor((Math.random() * 100)+1);
        	//document.write(correct);
+       	var guess = "";
+
+function yourGuess()
+	{
+    var guess =  $('#usernumber').val();
+    //document.getElementById("userguess").value;
+
+    // Appends guess to previous guesses list
+    function prevGuesses(guess) {
+    	$('#prev_guesses').prepend("<li>" + guess + "</li>");
+    }
+    
+
+	    
+	if (guess >100 || guess<1 || isNaN(guess)) {
+		
+    	$('#answer').html("Please enter a whole number between 1 and 100");
+    }	else {
+
+    var difference = Math.abs(guess - correct);
+    if (difference <= 3 && difference >= 1){
+    	$('#answer').html('<span id="scorching">Scorching!</span>');
+    		//alert("Scorching!");
+    	} else if (difference <= 10 && difference > 3) {
+    		$('#answer').html('<span id="hot">Hot!</span>');
+    		//alert("Hot!");
+    	} else if (difference <= 20 && difference > 10) {
+    		$('#answer').html('<span id="warm">Warm!</span>');
+    		//alert("Warm!");
+		} else if (difference <= 30 && difference > 20) {
+			$('#answer').html('<span id="cool">Cool!</span>');
+    		//alert("Cool!");
+    	} else if (difference <= 50 && difference > 30) {
+    		$('#answer').html('<span id="cold">Cold!</span>');
+    		//alert("Cold!");
+    	} else if (difference > 50) {
+    		$('#answer').html('<span id="freezing">Freezing!</span>');
+    		//alert("Freezing!");
+    	} else if (guess == correct) {
+    		$('#answer').html("Good guess! You guessed " + guess + " and the correct number is " + correct + "!!!");
+    		//alert("Good guess! You guessed " + guess + " and the correct number is " + correct );
+    	}
 
 
-       	function newGame()
-            {
-            correct = Math.floor((Math.random() * 100)+1);
-            
-            }
-       
-        function yourGuess()
-        	{
-            var guess =  document.getElementById("userguess").value;
-            if (guess >100 || guess<1 || isNaN(guess)) {
-            	alert("Please enter a whole number between 1 and 100");
-            	} else if (Math.abs(guess - correct) <= 3 && Math.abs(guess - correct) >= 1){
-            		alert("Scorching!");
-            	} else if (Math.abs(guess - correct) <= 10 && Math.abs(guess - correct) > 3) {
-            		alert("Hot!");
-            	} else if (Math.abs(guess - correct) <= 20 && Math.abs(guess - correct) > 10) {
-            		alert("Warm!");
-				} else if (Math.abs(guess - correct) <= 30 && Math.abs(guess - correct) > 20) {
-            		alert("Cool!");
-            	} else if (Math.abs(guess - correct) <= 50 && Math.abs(guess - correct) > 30) {
-            		alert("Cold!");
-            	} else if (Math.abs(guess - correct) > 50) {
-            		alert("Freezing!");
-            	} else if (guess == correct) {
-            		alert("Good guess! You guessed " + guess + " and the correct number is " + correct );
-            	}
-            }
+    	prevGuesses(guess);
+    }
+}
 
+
+/*
+// difference between last guess and previous guess
+    function difference(a,b) {
+		return Math.abs(a - b)
+	}; 
+
+}
+
+
+
+
+function newGame(){
+	    correct = Math.floor((Math.random() * 100)+1);
+	    }
+
+*/
--- guess2.js
+++ guess2.js
@@ -1,65 +1,24 @@
-$(document).ready(function(){
  var correct = Math.floor((Math.random() * 100)+1);
- //document.write(correct);
- var answer = "";
- //document.write(guess);
- var numGuesses = 0;
-
- //function tries(numGuesses){
-  //  numGuesses = numGuesses + 1;
-   // document.write(numGuesses);}
-
- function guess(){
-    var answer = $('#userguess').val();
-    if (answer >100 || answer<1 || isNaN(answer)) {
-                alert("Please enter a whole number between 1 and 100");
-                }
- }  
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-
-  var correct = Math.floor((Math.random() * 100)+1);
-       	//document.write(correct);
+           //document.write(correct);
        
         function yourGuess()
-        	{
-            var guess =  document.getElementById("userguess").value;
+           {
+            var guess =  document.getElementById("usernumber").value;
             if (guess >100 || guess<1 || isNaN(guess)) {
-            	alert("Please enter a whole number between 1 and 100");
-            	} else if (Math.abs(guess - correct) <= 3 && Math.abs(guess - correct) >= 1){
-            		("Scorching!");
-            	} else if (Math.abs(guess - correct) <= 10 && Math.abs(guess - correct) > 3) {
-            		alert("Hot!");
-            	} else if (Math.abs(guess - correct) <= 20 && Math.abs(guess - correct) > 10) {
-            		alert("Warm!");
-				} else if (Math.abs(guess - correct) <= 30 && Math.abs(guess - correct) > 20) {
-            		alert("Cool!");
-            	} else if (Math.abs(guess - correct) <= 50 && Math.abs(guess - correct) > 30) {
-            		alert("Cold!");
-            	} else if (Math.abs(guess - correct) > 50) {
-            		alert("Freezing!");
-            	} else if (guess == correct) {
-            		alert("Good guess! You guessed " + guess + " and the correct number is " + correct );
-            	}
-            }
-
-            function newGame(){
-            var correct = Math.floor((Math.random() * 100)+1);
-            }
-
-            */
+               alert("Please enter a whole number between 1 and 100");
+               } else if (Math.abs(guess - correct) <= 3 && Math.abs(guess - correct) > 0){
+                   ("Scorching!");
+               } else if (Math.abs(guess - correct) <= 10 && Math.abs(guess - correct) > 3) {
+                   alert("Hot!");
+               } else if (Math.abs(guess - correct) <= 20 && Math.abs(guess - correct) > 10) {
+                   alert("Warm!");
+               } else if (Math.abs(guess - correct) <= 30 && Math.abs(guess - correct) > 20) {
+                   alert("Cool!");
+               } else if (Math.abs(guess - correct) <= 50 && Math.abs(guess - correct) > 30) {
+                   alert("Cold!");
+               } else if (Math.abs(guess - correct) > 50) {
+                   alert("Freezing!");
+               } else if (guess == correct) {
+                   alert("Good guess! You guessed " + guess + " and the correct number is " + correct );
+               }
+            } 
