8f8182ff8f8d5ed8884a321b3a5f1988a1c580d5
tylerstauss <tylerstauss@Tylers-Mac-mini.local>
2013-10-22 10:40:29 -0700

    Working Javascript
    Site basically works as needed, with alerts for temperature.

--- guess.js
+++ guess.js
@@ -1,5 +1,24 @@
-var correct = Math.floor((Math.random() * 100)+1);
-
-var guess = document.getElementById("userguess").value;
-
-console.log(guess);
+  var correct = Math.floor((Math.random() * 100)+1);
+       	//document.write(correct);
+       
+        function yourGuess()
+        	{
+            var guess =  document.getElementById("userguess").value;
+            if (guess >100 || guess<1 || isNaN(guess)) {
+            	alert("Please enter a whole number between 1 and 100");
+            	} else if (Math.abs(guess - correct) <= 3 && Math.abs(guess - correct) > 0){
+            		("Scorching!");
+            	} else if (Math.abs(guess - correct) <= 10 && Math.abs(guess - correct) > 3) {
+            		alert("Hot!");
+            	} else if (Math.abs(guess - correct) <= 20 && Math.abs(guess - correct) > 10) {
+            		alert("Warm!");
+				} else if (Math.abs(guess - correct) <= 30 && Math.abs(guess - correct) > 20) {
+            		alert("Cool!");
+            	} else if (Math.abs(guess - correct) <= 50 && Math.abs(guess - correct) > 30) {
+            		alert("Cold!");
+            	} else if (Math.abs(guess - correct) > 50) {
+            		alert("Freezing!");
+            	} else if (guess == correct) {
+            		alert("Good guess! You guessed " + guess + " and the correct number is " + correct );
+            	}
+            }
